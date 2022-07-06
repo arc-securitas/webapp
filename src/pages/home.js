03ae7ab44fde88584c0a2d63f77506cc0588e901
a380ask <a380ask@outlook.com>
2022-07-05 21:59:44 -0700

    made some minor edits to home and about pages

--- src/pages/home.js
+++ src/pages/home.js
@@ -25,54 +25,55 @@ const Home = () => {
                         <img className="img1" src={"mockup.svg"} />
                     </div>
                 </section>
-
-                <section id="section2">
-                    <div className='left'>
-                        <div className='insideContent'>
-                            <p className='h2s'>The best way to stay safe is to stay connected.</p>
-                            <p className='otherContent'>Automatically check in with friends, family, and coworkers when you add them to your safety network!</p>
+                <div id="suboverall">
+                    <section id="section2">
+                        <div className='left'>
+                            <div className='insideContent'>
+                                <p className='h2s'>The best way to stay safe is to stay connected.</p>
+                                <p className='otherContent'>Automatically check in with friends, family, and coworkers when you add them to your safety network!</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className='right'>
-                        <img className="img2" src={"Group19.svg"} />
-                    </div>
-                </section>
-
-                <section id="section3">
-                    <div className='right'>
-                        <div className='insideContent'>
-                            <p className='h2s'>Stay informed of your team’s safety.</p>
-                            <p className='otherContent'>Our web app allows managing brokers to see all agent safey metrics and easily manage their subscriptions.</p>
+                        <div className='right'>
+                            <img className="img2" src={"Group19.svg"} />
                         </div>
-                    </div>
-                    <div className='left'>
-                        <img className="img3" src={"Group22.svg"} />
-                    </div>
-                </section>
+                    </section>
 
-                <section id="section4">
-                    <div className='left'>
-                        <div className='insideContent'>
-                            <p className='h2s'>Verify that your agents are safe, minute by minute.</p>
-                            <p className='otherContent'>With Arc’s safety timer, agents can periodically check in on their own customized schedule to stay safe without sacrificing productivity. Schedule safety timers easily when you connect your calendar!</p>
+                    <section id="section3">
+                        <div className='right'>
+                            <div className='insideContent'>
+                                <p className='h2s'>Stay informed of your team’s safety.</p>
+                                <p className='otherContent'>Our web app allows managing brokers to see all agent safey metrics and easily manage their subscriptions.</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className='right'>
-                        <img className="img4" src={"Group15.svg"} />
-                    </div>
-                </section>
+                        <div className='left'>
+                            <img className="img3" src={"Group22.svg"} />
+                        </div>
+                    </section>
 
-                <section id="section5">
-                    <div className='right'>
-                        <div className='insideContent'>
-                            <p className='h2s'>Help your agents the right way, right away.</p>
-                            <p className='otherContent'>If you feel that you might be in danger, send an alert with a single click. Your safety network will be notified with your most recent location, your calendar event details, and a short audio clip to contextualize the situation.</p>
+                    <section id="section4">
+                        <div className='left'>
+                            <div className='insideContent'>
+                                <p className='h2s'>Verify that your agents are safe, minute by minute.</p>
+                                <p className='otherContent'>With Arc’s safety timer, agents can periodically check in on their own customized schedule to stay safe without sacrificing productivity. Schedule safety timers easily when you connect your calendar!</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className='left'>
-                        <img className="img5" src={"Group17.svg"} />
-                    </div>
-                </section>
+                        <div className='right'>
+                            <img className="img4" src={"Group15.svg"} />
+                        </div>
+                    </section>
+
+                    <section id="section5">
+                        <div className='right'>
+                            <div className='insideContent'>
+                                <p className='h2s'>Help your agents the right way, right away.</p>
+                                <p className='otherContent'>If you feel that you might be in danger, send an alert with a single click. Your safety network will be notified with your most recent location, your calendar event details, and a short audio clip to contextualize the situation.</p>
+                            </div>
+                        </div>
+                        <div className='left'>
+                            <img className="img5" src={"Group17.svg"} />
+                        </div>
+                    </section>
+                </div>
             </div>
             <div id="section6">
                 <p className="h3s">Step into the future of real estate safety.</p>
