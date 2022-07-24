88a25ef1eb18ebbceaf1d07dc2ca094ce5a72a62
Logan <logansw428@gmail.com>
2022-07-24 13:52:25 -0700

    Programmer adds alt text to images

--- src/pages/product.js
+++ src/pages/product.js
@@ -18,10 +18,10 @@ const Product = () => {
                         <p>You arrive at a showing or open house.</p>
                         <Carousel>
                             <CarouselItem>
-                                <img className="story-img" src={"product-graphic1.svg"} />
+                                <img className="story-img" src={"product-graphic1.svg"} alt={'Real estate agent getting out of her car in front of a showing, using Arc\'s safety app'} />
                             </CarouselItem>
                             <CarouselItem>
-                                <img className="story-img" src={"product-screenshot1.png"} />
+                                <img className="story-img" src={"product-screenshot1.png"} alt={'Screenshot of UI to set a new safety timer and screenshot of active timer'} />
                             </CarouselItem>
                         </Carousel>
                         <p>Before you get out of the car, you can easily start a timer with the interval of your choice. This is just how often you want the app to check in with you!</p>
@@ -29,10 +29,11 @@ const Product = () => {
                         <p>Proceed with your showing as usual.</p>
                         <Carousel>
                             <CarouselItem>
-                                <img className="story-img" src={"product-graphic2.svg"} />
+                                <img className="story-img" src={"product-graphic2.svg"} alt={'Real estate agent talking with client while phone buzzes'} />
                             </CarouselItem>
                             <CarouselItem>
-                                <img className="story-img" src={"product-screenshot2.png"} />
+                                {/* TODO: Fix this image, it reads "Enter safety code to cancel timer" but it should say "Enter safety code to confirm safety and restart timer." */}
+                                <img className="story-img" src={"product-screenshot2.png"} alt={'Screenshot of UI showing depleted timer and prompt to enter safety code to cancel timer'} />
                             </CarouselItem>
                         </Carousel>
                         <p>When your timer reaches zero, the app will send you a check-in notification. Simply enter your 4-digit safety code and the app will automatically restart the timer.</p>
@@ -40,20 +41,20 @@ const Product = () => {
                         <p>If you ever feel unsafe, simply let the check-in timer run out or tap the Early Alert button to  instantly notify everyone in your safety network that you may be in danger.</p>
                             <Carousel>
                                 <CarouselItem>
-                                    <img className="story-img" src={"product-graphic3.svg"} />
+                                    <img className="story-img" src={"product-graphic3.svg"} alt={'Real estate agent attacked by client'} />
                                 </CarouselItem>
                                 <CarouselItem>
-                                    <img className="story-img" src={"product-screenshot3.png"} />
+                                    <img className="story-img" src={"product-screenshot3.png"} alt={'Screenshot of UI showing danger notification'} />
                                 </CarouselItem>
                             </Carousel>
                         <p>Your safety network will receive your current location, a short audio clip, and the details of any ongoing calendar events.</p>
                     </section>
                     <section className="highlight">
-                        <img className="highlight-img" src={"product-cal.svg"} />
+                        <img className="highlight-img" src={"product-cal.svg"} alt={'Calendar graphic'} />
                         <p className="story-center">Never forget to start a timer by letting Arc automatically schedule them for you. Just sync with your work calendar and we'll do the rest.</p>
-                        <img className="highlight-img" src={"product-alert.svg"} />
+                        <img className="highlight-img" src={"product-alert.svg"} alt={'Danger icon surrounded by Arc logos'} />
                         <p className="story-center">Update your Safety Network at any time by adding contacts. All you need is a name and a phone number.</p>
-                        <img className="highlight-img" src={"product-audio.svg"} />
+                        <img className="highlight-img" src={"product-audio.svg"} alt={'Microphone icon'} />
                         <p className="story-center">Arc can record audio clips in the event of a presumed emergency to help others get a better understanding of the situation.</p>
                     </section>
                 </Tab>
