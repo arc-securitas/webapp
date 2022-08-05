ef0dd889bf7e50798fe699817c30ce1c4dfbf2a5
Logan <logansw428@gmail.com>
2022-08-05 12:03:43 -0700

    Resized headings

--- src/pages/about.js
+++ src/pages/about.js
@@ -25,15 +25,15 @@ const About = () => {
     return (
         <>
             <div className='content'>
-                <section>
-                    <p className='subheads'>We are <span className='blueStuff'>Arc Security</span>.</p>
+                <section className="">
+                    <p className='superhead'>We are <span className='blueStuff'>Arc Security</span>.</p>
                 </section>
                 <section>
                     <p className='subtitles'>OUR MISSION</p>
                     <p>We envision a world in which people can feel safe on the job. However, from our extensive research, we know that many real estate professionals fear for their personal safety. Yet there is no industry-wide solution for employee safety management. We streamline processes for both the brokerage and the agent, providing peace of mind for everyone. With Arc, you can prioritize safety without sacrificing productivity.</p>
                 </section>
                 <section>
-                    <p className='subheads'>We are <span className='blueStuff'>committed</span> to building a <span className='blueStuff'>safer world</span>.</p>
+                    <p className='subhead'>We are <span className='blueStuff'>committed</span> to building a <span className='blueStuff'>safer world</span>.</p>
                     <p>We were founded with the mission to <strong>raise the bar for personal safety</strong> in the real estate space.</p>
 
                     <p>When asked about her safety, one agent told us: “At the end of the day after many long hours of showing homes, I pour myself a glass of wine, sit down on the couch, and think about all the moments that things could have gone wrong.”</p>
@@ -48,7 +48,7 @@ const About = () => {
                     <p>Our community is made up of a large network of real estate professional across the West Coast and beyond. We recognize the unique needs of our users and continuously develop features that benefit you the way you want. We are proud of the work we do and the community we have built.</p>
                 </section>
                 <section>
-                    <p className='subheads'>Meet the Team</p>
+                    <p className='subhead'>Meet the Team</p>
                     <Row className='overallPpl1' xs={2} sm={5}>
                     {memdata.map((val, index) => (
                         <div className='overallPpl'>
