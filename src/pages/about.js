010860974e1e46c36057a59206d4f30a818a7f07
Logan <logansw428@gmail.com>
2022-08-09 11:58:51 -0700

    Implemented IntersectObserver for detecting when to play animations

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,16 @@
 import './about.css';
+import animations from '../animations/animations.module.css';
 import useGoogleSheets from 'use-google-sheets';
 
 import React from "react";
+import { useElementOnScreen } from '../animations/animationHooks.js';
 import Row from 'react-bootstrap/Row';
 
 const About = () => {
+    const titleOnScreen = useElementOnScreen();
+    const subtitleOnScreen = useElementOnScreen();
+    const pplOnScreen = useElementOnScreen();
+
     const { data, loading, error } = useGoogleSheets({
         apiKey: process.env.REACT_APP_API_KEY,
         sheetId: process.env.REACT_APP_SHEET_ID,
@@ -25,15 +31,17 @@ const About = () => {
     return (
         <>
             <div className='content'>
-                <section className="">
-                    <p className='superhead'><div className='surface'>We are</div> <div className='surfaceSlow'><span className='blueStuff'>Arc Security</span>.</div></p>
+                <section className='superheadSection'>
+                    <p className='superhead' ref={titleOnScreen.ref}><div className={titleOnScreen.isVisible ? animations.surface : "hidden"}>We are</div> <div className={titleOnScreen.isVisible ? animations.surfaceSlow : "hidden"}><span className='blueStuff'>Arc Security</span>.</div></p>
                 </section>
                 <section>
                     <p className='subtitles'>OUR MISSION</p>
                     <p>We envision a world in which people can feel safe on the job. However, from our extensive research, we know that many real estate professionals fear for their personal safety. Yet there is no industry-wide solution for employee safety management. We streamline processes for both the brokerage and the agent, providing peace of mind for everyone. With Arc, you can prioritize safety without sacrificing productivity.</p>
                 </section>
-                <section>
-                    <p className='subhead'>We are <span className='blueStuff'>committed</span> to building a <span className='blueStuff'>safer world</span>.</p>
+                <section className='subheadSection'>
+                    <div ref={subtitleOnScreen.ref} className={subtitleOnScreen.isVisible ? animations.surface : "hidden"}>
+                        <p className='subhead'>We are <span className='blueStuff'>committed</span> to building a <span className='blueStuff'>safer world</span>.</p>
+                    </div>
                     <p>We were founded with the mission to <strong>raise the bar for personal safety</strong> in the real estate space.</p>
 
                     <p>When asked about her safety, one agent told us: “At the end of the day after many long hours of showing homes, I pour myself a glass of wine, sit down on the couch, and think about all the moments that things could have gone wrong.”</p>
@@ -49,9 +57,9 @@ const About = () => {
                 </section>
                 <section>
                     <p className='subhead'>Meet the Team</p>
-                    <Row className='overallPpl1' xs={2} sm={5}>
+                    <Row className='overallPpl1' xs={2} sm={5} ref={pplOnScreen.ref}>
                     {memdata.map((val, index) => (
-                        <div className='overallPpl'>
+                        <div className={pplOnScreen.isVisible ? `overallPpl ${animations.surface}` : 'hidden'} style={{animationDelay: `${index*0.1}s`}}>
                             <img class="profileImg" src={val["Your professional picture for the Website"].replace("open?", "uc?export=view&")} alt={val["Your Name (As you would like it to appear on the website)"]}></img>
                             <p className='profileData1'><strong>{val["Your Name (As you would like it to appear on the website)"]}</strong></p>
                             <p className='profileData'>{val["Role"]}</p>
