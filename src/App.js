abd8599369fa201e82cc66ba84bb7e6b0868c89e
a380ask <a380ask@outlook.com>
2022-08-16 20:55:29 -0700

    added footer stuff and made it ready for 1st deployment

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Footer from "./components/Footer.js";
 import Home from "./pages/home.js";
 import About from "./pages/about.js";
 import Product from "./pages/product.js";
+// import CookiePolicy from "./pages/cookiePolicy.html";
 
 function App(props) {
   return (
@@ -19,6 +20,9 @@ function App(props) {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
         <Route exact path="/product" element={<Product />} />
+        <Route exact path="/legal/cookiePolicy" render={() => { window.location.href = "cookiePolicy.html" }} /> 
+        <Route exact path="/legal/terms-and-conditions" render={() => { window.location.href = "terms-and-conditions.html" }} /> 
+        <Route exact path="/legal/privacy-policy-web" render={() => { window.location.href = "privacy-policy-web.html" }} /> 
       </Routes>
       <Footer />
     </Router>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,31 +11,35 @@ import {
   BrowserRouter as Router,
   Routes,
   NavLink,
-  Route
+  Route,
+  useNavigate
 } from "react-router-dom";
 
 export default function Footer() {
+  const navigate = useNavigate();
   return (
-    <FooterDiv className='overallFooter' style={{backgroundColor: "Black"}}>
-      <img src={logoBlue} style={{marginBottom:"-0.025px"}}/>
+    <FooterDiv className='overallFooter' style={{ backgroundColor: "Black" }}>
+      <a href="/"><img src={logoBlue} style={{ marginBottom: "-0.025px" }} /></a>
       <ul className="sitemap">
-        <li>Product</li>
-        <li>About</li>
-        <li>Careers</li>
-        <li>FAQ</li>
-        <li>Privacy Policy</li>
-        <li>Terms &#38; Conditions</li>
-        <li>Cookie Policy</li>
+          <li><a style={{"textDecoration": "none", "color": "white"}} onClick={() => navigate("/Product")} href="">Product</a></li>
+          <li><a style={{"textDecoration": "none", "color": "white"}} onClick={() => navigate("/About")} href="">About</a></li>
+          {/* <li>Careers</li> */}
+          {/* <li>FAQ</li> */}
+          <li><a style={{"textDecoration": "none", "color": "white"}} target="_blank" href={process.env.PUBLIC_URL + "/privacy-policy-web.html"}>Privacy Policy</a></li>
+          {/* <li onClick={() => navigate("webapp/legal/privacy-policy-web.html")}>Privacy Policy</li> */}
+          <li><a style={{"textDecoration": "none", "color": "white"}} target="_blank" href={process.env.PUBLIC_URL + "/terms-and-conditions.html"} >Terms &#38; Conditions</a></li>
+          <li><a style={{"textDecoration": "none", "color": "white"}} target="_blank" href={process.env.PUBLIC_URL + "/cookiePolicy.html"}>Cookie Policy</a></li>
+
       </ul>
-      <ul className="socials">
+      <ul className="socials" style={{"marginTop": "0px"}}>
         <li>
-          <BsInstagram size={25} />
+        <a style={{"textDecoration": "none", "color": "white"}} href="https://www.instagram.com/arc.security/"><BsInstagram size={25} /></a>
         </li>
         <li>
-          <FaLinkedinIn size={25} />
+          <a style={{"textDecoration": "none", "color": "white"}} href="https://www.linkedin.com/company/arc-security"><FaLinkedinIn size={25} /></a>
         </li>
         <li>
-          <FaFacebookF size={25} />
+        <a style={{"textDecoration": "none", "color": "white"}} href="https://www.facebook.com/arcsecure.it"><FaFacebookF size={25} /></a>
         </li>
       </ul>
       <p className="cc"> &copy;2022 Arc Security. All rights reserved</p>
