1d47721c0410ddf682aacfffcc2743033b0bfee9
Khushaal Kurswani <lords194@gmail.com>
2022-08-27 13:18:34 -0700

    Modified catch-all request in server

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,11 @@ mongoose
     .catch((err) => console.error("Could not connect to MongoDB: ", err));
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '/public')));
+app.use(express.static(path.resolve(__dirname, '../build')));
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/public/index.html'));
+    res.sendFile(path.resolve(__dirname, '../build/index.html'));
 });
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
