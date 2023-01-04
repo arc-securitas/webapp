cfd9f3863c1a775208827b010799d7a7bc81e2be
Logan <logansw428@gmail.com>
2023-01-04 10:56:56 -0800

    Added manager update route

--- backend/routes/managerRoutes.js
+++ backend/routes/managerRoutes.js
@@ -3,8 +3,8 @@ const express = require("express");
 
 const managerRoutes = express.Router();
 
+const Manager = require('../schemas/manager.js');
 const ObjectId = require("mongodb").ObjectId;
-
 let db_connect = mongoose.connection.collection("managers");
 
 // Get all managers
@@ -21,6 +21,23 @@ managerRoutes.route("/managers/:email").get(function (req, res) {
         if (err) throw err;
         res.json(result);
     });
+});
+
+// Updates the given manager info (based on their email). Middle name is optional.
+managerRoutes.route("/managers/update/:email/:firstName/:middleName?/:lastName/:phoneNumber").post(function (req, res) {
+    db_connect.updateOne(
+        {email: req.params.email},
+        {$set: {user_metadata: {
+            firstName: req.params.firstName,
+            middleName: req.params.middleName == null ? "" : req.params.middleName,
+            lastName: req.params.lastName,
+            phoneNumber: req.params.phoneNumber
+        }}},
+        function (err, result) {
+            if (err) throw err;
+            res.json(result);
+        }
+    );
 });
 
 module.exports = managerRoutes;
